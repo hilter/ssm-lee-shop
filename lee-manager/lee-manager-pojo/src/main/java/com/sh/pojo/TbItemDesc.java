78b216abd780c4b1f48c21c01f7c266cdcfb6dc7
AmazeLee <911711054@qq.com>
2018-05-02 16:08:40 +0800

    工程改造完成

--- lee-manager/lee-manager-pojo/src/main/java/com/sh/pojo/TbItemDesc.java
+++ lee-manager/lee-manager-pojo/src/main/java/com/sh/pojo/TbItemDesc.java
@@ -1,8 +1,9 @@
 package com.sh.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TbItemDesc {
+public class TbItemDesc implements Serializable {
     private Long itemId;
 
     private Date created;
